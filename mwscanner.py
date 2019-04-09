6d0974f4aae088a370ea542cbc9a65166700615c
Guilherme Guy <guilhermeguy349@gmail.com>
2019-04-09 10:32:42 -0300

    Added discipline list reading from departament page

--- mwscanner.py
+++ mwscanner.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from requests import get
 import json
-from collection import dequeue
+from collections import deque
 
 BASE_URL = 'https://matriculaweb.unb.br/'
 CAMPUS = {
@@ -12,12 +12,13 @@ CAMPUS = {
 }
 
 
-class TableReader():
+class TableReader:
 
-    def readTableFromHTML(raw_html):
+    @staticmethod
+    def readSimpleTableFromHTML(raw_html):
 
         # Table head comment guide in table
-        table_head_list = dequeue()
+        table_head_list = deque()
         extracted_data = []
 
         # Select all the th in html parser
@@ -43,7 +44,53 @@ class TableReader():
 
                 # Take off the first element in list and adding
                 # in final from the same list (queue)
-                table_head_list.append(table_head_list.pop())
+                table_head_list.appendleft(table_head_list.pop())
+
+            # Verify if the current course attribute is empty,
+            # if not append in list of course
+            if attributes != {}:
+                extracted_data.append(attributes)
+
+        return extracted_data
+
+    @staticmethod
+    def readDatatableTableFromHTML(raw_html):
+
+        # Table head comment guide in table
+        table_head_list = deque()
+        extracted_data = []
+
+        database_div = raw_html.select('#datatable')
+
+        if len(database_div) == 0:
+            return None
+
+        # Select all the rows in html
+        for table_row in database_div[0].select('tr'):
+
+            if len(table_head_list) == 0:
+                for th in table_row.select('th'):
+                    table_head_list.append(str(th.text))
+                continue
+
+            attributes = {}
+
+            # In all rows we take the data
+            for table_data in table_row.select('td'):
+
+                if str(table_data.text) == '':
+                    break
+
+                # Creating the dictionarie with the
+                # first element in table head list
+                # and data table text
+                attributes[table_head_list[0]] = str(
+                    table_data.text
+                )
+
+                # Take off the first element in list and adding
+                # in final from the same list (queue)
+                table_head_list.appendleft(table_head_list.pop())
 
             # Verify if the current course attribute is empty,
             # if not append in list of course
@@ -66,7 +113,7 @@ class Course():
         self.disciplines = []
 
 
-class Department():
+class Department(TableReader):
 
     def __init__(self, campus, code, name, initials):
 
@@ -76,6 +123,22 @@ class Department():
         self.initials = initials
 
         self.disciplines = []
+        self.links = []
+
+    def getDisciplineListURL(self):
+        return BASE_URL + 'graduacao/oferta_dis.aspx?cod={}'.format(self.code)
+
+    def buildLinkList(self):
+
+        response = get(self.getDisciplineListURL())
+
+        if response.status_code == 200:
+
+            # Make the parse for html
+            raw_html = BeautifulSoup(response.content, 'html.parser')
+            table_data = self.readDatatableTableFromHTML(raw_html)
+
+            print(table_data)
 
     def getDisciplines():
         pass
@@ -104,7 +167,7 @@ class Campus(TableReader):
 
             # Make the parse for html
             raw_html = BeautifulSoup(response.content, 'html.parser')
-            table_data = self.readTableFromHTML(raw_html)
+            table_data = self.readSimpleTableFromHTML(raw_html)
 
             for data in table_data:
                 c = Course(
@@ -128,7 +191,7 @@ class Campus(TableReader):
 
             # Make the parse for html
             raw_html = BeautifulSoup(response.content, 'html.parser')
-            table_data = self.readTableFromHTML(raw_html)
+            table_data = self.readSimpleTableFromHTML(raw_html)
 
             for data in table_data:
 
@@ -136,7 +199,7 @@ class Campus(TableReader):
                     campus=campus_code,
                     code=data['Código'],
                     name=data['Denominação'],
-                    initial=data['Sigla']
+                    initials=data['Sigla']
                 )
                 self.departments.update({d.code: d})
 
@@ -202,6 +265,9 @@ class Class():
 if __name__ == '__main__':
     campus = Campus()
     list_all_campus_courses = campus.getAllCampusCourses()
-    print(list_all_campus_courses)
+    # print(list_all_campus_courses)
     list_all_campus_departments = campus.getAllCampusDepartments()
-    print(list_all_campus_departments)
+    # print(list_all_campus_departments)
+
+    for departament in campus.departments.values():
+        departament.buildLinkList()
