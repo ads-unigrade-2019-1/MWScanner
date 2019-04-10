5d2ce653e55fb87dddf0a336ce8ee7326c426b70
Joberth Rogers <joberth.rogers18@gmail.com>
2019-04-10 10:44:51 -0400

    Added comentaries in Campus class

--- mwscanner.py
+++ mwscanner.py
@@ -3,6 +3,8 @@ from requests import get
 import json
 
 BASE_URL = 'https://matriculaweb.unb.br/'
+
+# Campus index for guide in url search
 CAMPUS = {
     'Darcy Ribeiro': 1,
     'Planaltina': 2,
@@ -199,7 +201,9 @@ class Department(TableReader):
 
 
 class Campus(TableReader):
+# This class represent all the 4 campus prensent in UnB
 
+		# Define the atributes basics from all campus 
     def __init__(self):
         self.all_campus_courses = {}
         self.courses = []
@@ -207,23 +211,35 @@ class Campus(TableReader):
         self.all_campus_departments = {}
         self.departments = []
 
+		# This method return all the courses 
+		# present in campus pass by parameter
     def getCampusCoursesUrl(self, campus):
         return BASE_URL + 'graduacao/curso_rel.aspx?cod={}'.format(campus)
 
+		# This method return all the Departaments present 
+		# in campus passed by parameter
     def getCampusDepartmentsUrl(self, campus):
         return BASE_URL + 'graduacao/oferta_dep.aspx?cod={}'.format(campus)
 
+		# This method return the list of courses from campus
     def getCampusCourses(self, campus_code):
 
+				# Make the response according the campus URL and
+				# initiate the list of courses
         response = get(self.getCampusCoursesUrl(campus_code))
         list_courses = []
 
+				# if the status code is "ok"
         if response.status_code == 200:
 
-            # Make the parse for html
+            # Make the parse for html using beautifulsoap
+						# Read the data from table using the parser
             raw_html = BeautifulSoup(response.content, 'html.parser')
             table_data = self.readSimpleTableFromHTML(raw_html)
 
+						# According the row in each table we take the especific 
+						# colum in table and create a instance from course
+						# and seve in list courses from campus
             for data in table_data:
                 self.courses.append(
                     Course(
@@ -237,6 +253,9 @@ class Campus(TableReader):
                 list_courses.append(data)
         return list_courses
 
+		# This method using the function above to create
+		# the list of all courses and create an dict for
+		# that specific campus 
     def getAllCampusCourses(self):
 
         for campus, code in CAMPUS.items():
@@ -244,18 +263,25 @@ class Campus(TableReader):
                 campus: self.getCampusCourses(code)
             })
         return self.all_campus_courses
-    
+
+		# This method using the index campus and access		
+    # all the departament for that specifc campus
     def getCampusDepartments(self, campus_code):
 
+				# Make response and initialize the list of departaments
         response = get(self.getCampusDepartmentsUrl(campus_code))
         list_departments = []
 
+				# Verify if the status cod is ok
         if response.status_code == 200:
 
             # Make the parse for html
+						# And read the table indentify in parse html
             raw_html = BeautifulSoup(response.content, 'html.parser')
             table_data = self.readSimpleTableFromHTML(raw_html)
 
+						# For all row in table, an object Departament
+						# is create and added in list of departaments
             for data in table_data:
                 self.departments.append(
                     Department(
@@ -266,9 +292,12 @@ class Campus(TableReader):
                     )
                 )
                 list_departments.append(data)
+				
         return list_departments
 
-
+		# This method using the function above to create
+		# the list of all departaments and return an dictionary for
+		# that specific campus 
     def getAllCampusDepartments(self):
 
         for campus, code in CAMPUS.items():
