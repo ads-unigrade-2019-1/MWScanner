2500aec7f8d63bc1ea57ffe6b1ada9ee2734372d
DanMke <danmke@hotmail.com>
2019-04-10 12:18:38 -0300

    Corrected pep8 errors
    Signed-off-by: DanMke <danmke@hotmail.com>

--- mwscanner.py
+++ mwscanner.py
@@ -14,24 +14,24 @@ CAMPUS = {
 
 
 class TableReader:
-# Abstract class that provides methods for the 
-# reading of tables on pages of Matricula Web 
-# website. This is inherithed in a scheme that allow
-# multiple inheritance, thus acting, in a way, similarly
-# to Java Interfaces.
+    # Abstract class that provides methods for the
+    # reading of tables on pages of Matricula Web
+    # website. This is inherithed in a scheme that allow
+    # multiple inheritance, thus acting, in a way, similarly
+    # to Java Interfaces.
 
     @staticmethod
     def readSimpleTableFromHTML(raw_html):
-    # This method is capable of reading tables on
-    # pages that use the common layout on Matricula
-    # Web. It recieves the raw_html element (returned from
-    # BeautifulSoup) and process it.
-    # It returns a list in which each element is a dictionarie
-    # that has the following structure:
-    # {
-    #   'table_head_name': 'table_row_attribute',
-    #   ...
-    # }
+        # This method is capable of reading tables on
+        # pages that use the common layout on Matricula
+        # Web. It recieves the raw_html element (returned from
+        # BeautifulSoup) and process it.
+        # It returns a list in which each element is a dictionarie
+        # that has the following structure:
+        # {
+        #   'table_head_name': 'table_row_attribute',
+        #   ...
+        # }
 
         # Table head comment guide in table
         table_head_list = []
@@ -71,16 +71,16 @@ class TableReader:
 
     @staticmethod
     def readDatatableTableFromHTML(raw_html):
-    # This method is capable of reading tables on
-    # pages that use the datatable layout on Matricula
-    # Web. It recieves the raw_html element (returned from
-    # BeautifulSoup) and process it.
-    # It returns a list in which each element is a dictionarie
-    # that has the following structure:
-    # {
-    #   'table_head_name': 'table_row_attribute',
-    #   ...
-    # }
+        # This method is capable of reading tables on
+        # pages that use the datatable layout on Matricula
+        # Web. It recieves the raw_html element (returned from
+        # BeautifulSoup) and process it.
+        # It returns a list in which each element is a dictionarie
+        # that has the following structure:
+        # {
+        #   'table_head_name': 'table_row_attribute',
+        #   ...
+        # }
 
         # Table head comment guide in table
         table_head_list = []
@@ -127,8 +127,8 @@ class TableReader:
 
 
 class Course():
-# This class represents a course registered on the Matricula
-# Web. It has the information about this course.
+    # This class represents a course registered on the Matricula
+    # Web. It has the information about this course.
 
     def __init__(self, campus, code, name, shift, modality):
 
@@ -141,7 +141,7 @@ class Course():
         # Course name
         self.name = name
 
-        # Course shift (Ex: 'Matutino', 'Integral')
+        # Course shift (Ex: 'Diurno', 'Noturno')
         self.shift = shift
 
         # Type of degree this course provides
@@ -150,7 +150,8 @@ class Course():
         # This represents the disciplines associated with this
         # course. The data here wiil be on the following format:
         # disciplines == [
-        #   'PERIOD_NUMBER': [list with the code for the discipline of this period]
+        #   'PERIOD_NUMBER': [list with the code for the
+        #                     discipline of this period]
         #   ...
         # ]
         self.disciplines = []
@@ -172,9 +173,9 @@ class Department(TableReader):
         return BASE_URL + 'graduacao/oferta_dis.aspx?cod={}'.format(self.code)
 
     def buildLinkList(self):
-    # This method builds the list of disciplines that belongs
-    # to this departament. This list will be later used to
-    # process the creation of the Discipline object.
+        # This method builds the list of disciplines that belongs
+        # to this departament. This list will be later used to
+        # process the creation of the Discipline object.
 
         response = get(self.getDisciplineListURL())
 
@@ -194,16 +195,17 @@ class Department(TableReader):
 
             # the table_data can be empty
             if table_data is not None:
-              self.unprocessedDisciplines += [
-                {'Código': x['Código'], 'Denominação': x['Denominação']}
-                for x in table_data
-              ]
+                self.unprocessedDisciplines += [
+                    {'Código': x['Código'], 'Denominação': x['Denominação']}
+                    for x in table_data
+                ]
+                print(self.unprocessedDisciplines)
 
 
 class Campus(TableReader):
-# This class represent all the 4 campus prensent in UnB
+    # This class represent all the 4 campus prensent in UnB
 
-		# Define the atributes basics from all campus 
+    # Define the atributes basics from all campus
     def __init__(self):
         self.all_campus_courses = {}
         self.courses = []
@@ -211,51 +213,51 @@ class Campus(TableReader):
         self.all_campus_departments = {}
         self.departments = []
 
-		# This method return all the courses 
-		# present in campus pass by parameter
+    # This method return all the courses
+    # present in campus pass by parameter
     def getCampusCoursesUrl(self, campus):
         return BASE_URL + 'graduacao/curso_rel.aspx?cod={}'.format(campus)
 
-		# This method return all the Departaments present 
-		# in campus passed by parameter
+    # This method return all the Departaments present
+    # in campus passed by parameter
     def getCampusDepartmentsUrl(self, campus):
         return BASE_URL + 'graduacao/oferta_dep.aspx?cod={}'.format(campus)
 
-		# This method return the list of courses from campus
+    # This method return the list of courses from campus
     def getCampusCourses(self, campus_code):
 
-				# Make the response according the campus URL and
-				# initiate the list of courses
+        # Make the response according the campus URL and
+        # initiate the list of courses
         response = get(self.getCampusCoursesUrl(campus_code))
         list_courses = []
 
-				# if the status code is "ok"
+        # if the status code is "ok"
         if response.status_code == 200:
 
             # Make the parse for html using beautifulsoap
-						# Read the data from table using the parser
+            # Read the data from table using the parser
             raw_html = BeautifulSoup(response.content, 'html.parser')
             table_data = self.readSimpleTableFromHTML(raw_html)
 
-						# According the row in each table we take the especific 
-						# colum in table and create a instance from course
-						# and seve in list courses from campus
+            # According the row in each table we take the specific
+            # colum in table and create a instance from course
+            # and seve in list courses from campus
             for data in table_data:
                 self.courses.append(
                     Course(
-                    campus=campus_code,
-                    code=data['Código'],
-                    name=data['Denominação'],
-                    shift=data['Turno'],
-                    modality=data['Modalidade']
+                        campus=campus_code,
+                        code=data['Código'],
+                        name=data['Denominação'],
+                        shift=data['Turno'],
+                        modality=data['Modalidade']
                     )
                 )
                 list_courses.append(data)
         return list_courses
 
-		# This method using the function above to create
-		# the list of all courses and create an dict for
-		# that specific campus 
+    # This method using the function above to create
+    # the list of all courses and create an dict for
+    # that specific campus
     def getAllCampusCourses(self):
 
         for campus, code in CAMPUS.items():
@@ -264,40 +266,40 @@ class Campus(TableReader):
             })
         return self.all_campus_courses
 
-		# This method using the index campus and access		
+    # This method using the index campus and access
     # all the departament for that specifc campus
     def getCampusDepartments(self, campus_code):
 
-				# Make response and initialize the list of departaments
+        # Make response and initialize the list of departaments
         response = get(self.getCampusDepartmentsUrl(campus_code))
         list_departments = []
 
-				# Verify if the status cod is ok
+        # Verify if the status cod is ok
         if response.status_code == 200:
 
             # Make the parse for html
-						# And read the table indentify in parse html
+            # And read the table indentify in parse html
             raw_html = BeautifulSoup(response.content, 'html.parser')
             table_data = self.readSimpleTableFromHTML(raw_html)
 
-						# For all row in table, an object Departament
-						# is create and added in list of departaments
+            # For all row in table, an object Departament
+            # is create and added in list of departaments
             for data in table_data:
                 self.departments.append(
                     Department(
-                    campus=campus_code,
-                    code=data['Código'],
-                    name=data['Denominação'],
-                    initials=data['Sigla']
+                        campus=campus_code,
+                        code=data['Código'],
+                        name=data['Denominação'],
+                        initials=data['Sigla']
                     )
                 )
                 list_departments.append(data)
-				
+
         return list_departments
 
-		# This method using the function above to create
-		# the list of all departaments and return an dictionary for
-		# that specific campus 
+    # This method using the function above to create
+    # the list of all departaments and return an dictionary for
+    # that specific campus
     def getAllCampusDepartments(self):
 
         for campus, code in CAMPUS.items():
@@ -308,26 +310,25 @@ class Campus(TableReader):
 
 
 class Discipline():
-# This class represents a Discipline present on
-# matriculaweb. it contains data about the discipline
-# and holds its classes and requirements
+    # This class represents a Discipline present on
+    # matriculaweb. it contains data about the discipline
+    # and holds its classes and requirements
 
     def __init__(self, code, name, departament, discipline_credits, category):
 
         # name of the discipline
         self.name = name
 
-        # identificator code for the discipline 
+        # identificator code for the discipline
         # (it's unique among disciplines)
         self.code = code
-        
+
         # departament to which this discipline belongs
         self.departament = departament
 
         # aumount of credits that this discipline
         # is worth
         self.credits = discipline_credits
-
 
         self.category = category
 
@@ -346,10 +347,9 @@ class Discipline():
 
 
 class Class():
-# This class represents a discipline class on Matricula
-# Web. It contains data about the meetings and a
-# connection to the discipline.
-
+    # This class represents a discipline class on Matricula
+    # Web. It contains data about the meetings and a
+    # connection to the discipline.
 
     def __init__(self, name, vacancies, discipline):
 
@@ -372,12 +372,11 @@ class Class():
         self.meetings = []
 
     def appendMeeting(self, room, day, hour):
-    # adds a meeting to this class list of meetings
-    # all parameters are strings
-    # room: room where the meeting will occour
-    # day: weekday of the meeting
-    # hour: time of the meeting
-
+        # adds a meeting to this class list of meetings
+        # all parameters are strings
+        # room: room where the meeting will occour
+        # day: weekday of the meeting
+        # hour: time of the meeting
 
         self.meetings.append({
             'room': room,
@@ -396,13 +395,14 @@ if __name__ == '__main__':
     # frow the Web
     list_all_campus_courses = campus.getAllCampusCourses()
     list_all_campus_departments = campus.getAllCampusDepartments()
-    
+
     # pprint all courses found
     for course in campus.courses:
-      print("COURSE - COD: {} NOME: {}".format(course.code, course.name))
+        print("[CURSO] CÓDIGO: {} - NOME: {}".format(course.code, course.name))
 
     # prints departament information
     # and then build the list of disciplines that each departament have
     for departament in campus.departments:
-        print("DEP - COD: {} NOME: {}".format(departament.code, departament.name))
+        print("[DEPARTAMENTO] CÓDIGO: {} - NOME: {}".format(
+                departament.code, departament.name))
         departament.buildLinkList()
