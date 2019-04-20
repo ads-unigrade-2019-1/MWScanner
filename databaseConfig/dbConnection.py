92d5f923d2661cacaba2f38f2cdd0802b7efc310
Guilherme Guy <guilhermeguy349@gmail.com>
2019-04-20 09:05:11 -0300

    Added ability to read database config from system env

--- databaseConfig/dbConnection.py
+++ databaseConfig/dbConnection.py
@@ -1,10 +1,21 @@
 from pymongo import MongoClient
+import os
+
 
 class Database:
 
     @staticmethod
     def defineConnections():
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client['matriculaweb']
-        return db
 
+        url = 'mongodb://localhost:27017/'
+        client_name = 'matriculaweb'
+
+        if 'DB_URL' in os.environ:
+            url = os.environ['DB_URL']
+
+        if 'DB_CLIENT_NAME' in os.environ:
+            client_name = os.environ['DB_CLIENT_NAME']
+
+        client = MongoClient(url)
+        db = client[client_name]
+        return db
